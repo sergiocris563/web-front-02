af45e436c91d69f3f71bfc54cbc1fef05200a161
Logon Aluno <logonrmlocal@fiap.com.br>
2023-10-20 11:10:37 -0300

    Adicionando exemplo de for

---
+++ exemplo2-for.js
@@ -0,0 +1,8 @@
+const frutas = ["maça", "banana", "uva", "pera"];
+
+// console.table(frutas.length) - realiza a contagem de itens dentro da lista
+
+for (let i = 0; i < frutas.length; i++) {
+    // console.log(frutas[i]);
+    console.log(`${i} - ${frutas[i]}`);
+}
