29922705ff968dba38b1bf863d38b3dfc016e717
Logon Aluno <logonrmlocal@fiap.com.br>
2023-10-27 11:47:04 -0300

    Apenas adicionando as coisas

---
+++ js/dom.js
@@ -0,0 +1,9 @@
+// criando variaveis
+const tirarDiv = document.querySelector(".tirarDiv");
+const voltarDiv = document.querySelector(".voltarDiv");
+const card = document.querySelector(".card");
+
+// criando funções
+tirarDiv.addEventListener("click", function() {
+    alert("testeee");
+})
