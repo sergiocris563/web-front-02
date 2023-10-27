9471cf0cdd313d049291db8ea1f1b26a76ade148
Logon Aluno <logonrmlocal@fiap.com.br>
2023-10-27 10:52:51 -0300

    Adicionando coisas para o console da html

---
+++ js/funcao.js
@@ -0,0 +1,21 @@
+function teste() {
+    console.log("Oláaaa");
+}
+
+function meuNome(name) {
+    console.log(`Olá ${name}`);
+}
+
+function calc(arg1, arg2) {
+    return arg1 + arg2;
+}
+
+console.log(calc(4, 4));
+console.log(calc(4, 4));
+console.log(calc(4, 4));
+
+meuNome("João");
+meuNome("Giovanni");
+meuNome("Carla");
+
+teste();    
