435429cdbe51a2aac9927c05d5635f01dd1536f9
Logon Aluno <logonrmlocal@fiap.com.br>
2023-11-10 11:24:19 -0300

    essa poha ate valeu a pena

--- js/script.js
+++ js/script.js
@@ -1,6 +1,18 @@
 const buttonMedicamentos = document.querySelector("#medicamentos");
 const listaRemedios = document.querySelector(".reremedios")
 
+// variaveis de validação
+const campoNome = document.querySelector("#nome");
+const campoCidade = document.querySelector("#cidade");
+const buttonValidacao = document.querySelector("#button");
+
+// função da validação
+buttonValidacao.addEventListener("click", function(event){
+    event.preventDefault();
+    console.log("aqui é o evento", event);
+    console.log(campoNome.value);
+});
+
 buttonMedicamentos.addEventListener("click", function() {
-    alert("clicou");
+    listaRemedios.classList.add("d-block");
 });
